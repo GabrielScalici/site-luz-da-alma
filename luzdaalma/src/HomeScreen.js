933421d4c7ec4f5a7575e0386bfc906b2d17f87d
GabrielScalici <gabrielhc.scalici@gmail.com>
2019-06-01 00:48:41 -0300

    semantic e bootstrap

--- luzdaalma/src/HomeScreen.js
+++ luzdaalma/src/HomeScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 //import logo from './logo.svg';
-import { Container, Row, Col, Navbar, Nav, Form, FormControl, Button, Card, Transformation } from 'react-bootstrap';
+import { Container, Row, Col, Navbar, Nav, Button, Jumbotron } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 import './App.css';
 
 import logo from './img/logo_lateral.png';
 import banner from './img/banner.png';
+import teste from './img/teste.png';
 
 function HomeScreen() {
     return (
@@ -27,16 +28,27 @@ function HomeScreen() {
         </a>
             </header> */}
             <Image sm={12} fluid className="Banner-img" src={banner} />
-            <Image sm={12} fluid className="Banner-img" src={banner} />
-            <Image sm={12} fluid className="Banner-img" src={banner} />
-            <Image sm={12} fluid className="Banner-img" src={banner} />
-            <Image sm={12} fluid className="Banner-img" src={banner} />
-            <Image sm={12} fluid className="Banner-img" src={banner} />
-            <Image sm={12} fluid className="Banner-img" src={banner} />
-            <Image sm={12} fluid className="Banner-img" src={banner} />
             <Container>
-            </Container>
+                <div class="ui two column grid">
+                    <div class="column">
+                        <div class="ui fluid image">
+                            <div class="ui black ribbon label">
+                                <i class="hotel icon"></i> Hotel
+                            </div>
+                            <img src={teste} />
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="ui fluid image">
+                            <div class="ui blue ribbon label">
+                                <i class="spoon icon"></i> Food
+                            </div>
+                            <img src={teste} />
+                        </div>
+                    </div>
+                </div>
 
+            </Container>
 
         </div>
     );
