166e5492c9c30a6c558ebc697ee23e56a1f8ff3b
GabrielScalici <gabrielhc.scalici@gmail.com>
2019-05-31 22:26:15 -0300

    rota criada

--- luzdaalma/src/App.js
+++ luzdaalma/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Navbar, Nav, Image } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//TELAS
+import ArtigosScreen from './ArtigosScreen';
+import HomeScreen from './HomeScreen';
+
+//IMAGENS
+import logo from './img/logo_lateral.png';
+
+class App extends Component {
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Navbar bg="white" variant="light">
+                        <Navbar.Brand href="#home">
+                            <Image className="Logo-nav" src={logo} />
+                        </Navbar.Brand>
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="/artigosscreen">Artigos</Nav.Link>
+                            <Nav.Link href="#eventos">Eventos</Nav.Link>
+                            <Nav.Link href="#tv">TV Espírita</Nav.Link>
+
+                        </Nav>
+                        {/* <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-primary">Search</Button>
+                </Form> */}
+                    </Navbar>
+                    <Switch>
+                        <Route exact path='/' component={HomeScreen} />
+                        <Route path='/artigosscreen' component={ArtigosScreen} />
+                    </Switch>
+                </div>
+
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ luzdaalma/src/ArtigosScreen.js
@@ -0,0 +1,94 @@
+import React, {Component} from 'react';
+//import logo from './logo.svg';
+import { Container, Row, Col, Navbar, Nav, Form, FormControl, Button, Card, Transformation, Carousel } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image';
+import './App.css';
+
+import logo from './img/logo_lateral.png';
+import banner from './img/banner.png';
+
+class ArtigosScreen extends Component {
+
+    render() {
+        return (
+            <div>
+            </div>
+
+        );
+    }
+}
+
+export default ArtigosScreen;
+
+// function ArtigosScreen() {
+//     return (
+//         <div className="App">
+//             {/* <header className="App-header">
+//                 <img src={logo} className="App-logo" alt="logo" />
+//                 <p>
+//                     Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//                 <p className="Texto-teste"> Olá meus amigosssssssss </p>
+//                 <p> Olá meus amigosssssssss </p>
+//                 <a
+//                     className="App-link"
+//                     href="https://reactjs.org"
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                 >
+//                     Learn React
+//         </a>
+//             </header> */}
+//             <Carousel
+//                 //activeIndex={index}
+//                 //direction={direction}
+//                 onSelect={this.handleSelect}
+//             >
+//                 <Carousel.Item>
+//                     <img
+//                         className="d-block w-100"
+//                         src="holder.js/800x400?text=First slide&bg=373940"
+//                         alt="First slide"
+//                     />
+//                     <Carousel.Caption>
+//                         <h3>First slide label</h3>
+//                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+//                     </Carousel.Caption>
+//                 </Carousel.Item>
+//                 <Carousel.Item>
+//                     <img
+//                         className="d-block w-100"
+//                         src="holder.js/800x400?text=Second slide&bg=282c34"
+//                         alt="Third slide"
+//                     />
+
+//                     <Carousel.Caption>
+//                         <h3>Second slide label</h3>
+//                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+//                     </Carousel.Caption>
+//                 </Carousel.Item>
+//                 <Carousel.Item>
+//                     <img
+//                         className="d-block w-100"
+//                         src="holder.js/800x400?text=Third slide&bg=20232a"
+//                         alt="Third slide"
+//                     />
+
+//                     <Carousel.Caption>
+//                         <h3>Third slide label</h3>
+//                         <p>
+//                             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//             </p>
+//                     </Carousel.Caption>
+//                 </Carousel.Item>
+//             </Carousel>
+//             <Container>
+//             </Container>
+
+
+//         </div>
+//     );
+// }
+
+// export default ArtigosScreen;
